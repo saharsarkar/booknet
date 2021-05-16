7968811ea0dc940fbf224f5b097a271c1b9c95f8
saharsarkar <sarkarsahar@gmail.com>
2021-05-16 12:10:39 +0430

    Add seeder for Author,  Category, and Publisher

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         // Call seeders
         $this->call([
             UserSeeder::class,
+            AuthorPublisherCategorySeeder::class,
         ]);
     }
 }
